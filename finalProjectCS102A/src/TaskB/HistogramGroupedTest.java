1b6bfb10d53a98a2639ca223f7c49670f7542e1b
WSC <WSC@10.21.23.121>
2018-12-03 15:41:27 +0800

    add group and stack test

---
+++ finalProjectCS102A/src/TaskB/HistogramGroupedTest.java
@@ -0,0 +1,10 @@
+package TaskB;
+
+public class HistogramGroupedTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ finalProjectCS102A/src/TaskB/HistogramStackedTest.java
@@ -0,0 +1,10 @@
+package TaskB;
+
+public class HistogramStackedTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
