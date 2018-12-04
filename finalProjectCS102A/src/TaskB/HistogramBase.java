016ddf960a31c5d2ac4ea7c18b714412879a9315
WSC <WSC@10.21.23.121>
2018-12-04 23:35:15 +0800

    Changes for base class
    #switch instance methods to abstract(because of stdDraw problem)

--- finalProjectCS102A/src/TaskB/HistogramBase.java
+++ finalProjectCS102A/src/TaskB/HistogramBase.java
@@ -123,82 +123,19 @@ public abstract class HistogramBase {
         }
     }
 
-    public void draw() {
-        setCanvas();
-        plotBars();
-        plotRuler();
-        plotKeys();
-        plotLegends();
-        if (f.hasBorder)
-            plotBorder();
-        if (f.hasRightRuler)
-            plotRightRuler();
-        if (f.hasHeader)
-            plotHeader();
-        if (f.hasFooter)
-            plotFooter();
-    }
+    public abstract void draw();
 
-    private void setCanvas() {
-        StdDraw.setCanvasSize(c.x, c.y);
-        setOriginalScale();
-        StdDraw.clear(c.bgColor);
-        StdDraw.setPenColor(c.color);
-    }
+    protected abstract void setCanvas();
 
-    protected void setHistogramScale(int nBars) {
-        double span = yValue[MAX] - yValue[MIN] + 1;
-        double ySpacing = span / (1 - f.margins[NORTH] - f.margins[SOUTH]);
-        yScale[MIN] = yValue[MIN] - f.margins[SOUTH] * ySpacing - 1;
-        yScale[MAX] = yValue[MAX] + f.margins[NORTH] * ySpacing;
-        StdDraw.setYscale(yScale[MIN], yScale[MAX]);
-
-        double xSpacing = (nBars + 1) / (1 - f.margins[WEST] - f.margins[EAST]);
-        xScale[MIN] = -f.margins[WEST] * xSpacing - 1;
-        xScale[MAX] = nBars + f.margins[EAST] * xSpacing;
-        StdDraw.setXscale(xScale[MIN], xScale[MAX]);
-    };
-
-    private void setOriginalScale() {
-        StdDraw.setXscale(c.xScale[MIN], c.xScale[MAX]);
-        StdDraw.setYscale(c.yScale[MIN], c.yScale[MAX]);
-    }
+    protected abstract void setHistogramScale(int nBars);
+    
+    protected abstract void setOriginalScale();
 
     protected abstract void plotBars();
 
-    protected void plotRuler() {
-        StdDraw.setFont(f.rulerFont);
-        StdDraw.setPenColor(f.rulerColor);
-        final double x0 = xValue[MIN] - 0.05, x1 = xValue[MIN] + 0.05;
-        String[] mark = new String[rulerGrade + 1];
-        for (int i = 0; i <= rulerGrade; i++) {
-            double y = yValue[MIN] + i * rulerStep;
-            mark[i] = numberForRuler(y);
-            StdDraw.line(x0, y, x1, y);
-        }
-        int len = maxMarkLength(mark);
-        final double xs = xScale[MIN] + 0.7 * (xValue[MIN] - xScale[MIN]);
-        for (int i = 0; i <= rulerGrade; i++) {
-            double y = yValue[MIN] + i * rulerStep;
-            StdDraw.text(xs, y, String.format("%" + len + "s", mark[i]));
-        }
-    }
+    protected abstract void plotRuler();
 
-    protected String numberForRuler(double x) { // TO BE Customized
-        if (yValue[MAX] >= 5 && rulerStep > 1)
-            return "" + (int) x;
-        if (rulerStep > 0.1)
-            return String.format("%.1f", x);
-        if (rulerStep > 0.01)
-            return String.format("%.2f", x);
-        if (rulerStep > 0.001)
-            return String.format("%.3f", x);
-        if (rulerStep > 0.0001)
-            return String.format("%.4f", x);
-        if (rulerStep > 0.00001)
-            return String.format("%.5f", x);
-        return String.format("%g", x);
-    }
+    protected abstract String numberForRuler(double x);
 
     private int maxMarkLength(String[] sa) {
         int n = sa[0].length();
@@ -208,70 +145,22 @@ public abstract class HistogramBase {
         return n;
     }
 
-    private void plotKeys() {
-        StdDraw.setFont(f.keysFont);
-        StdDraw.setPenColor(f.keyColor);
-        final double y = yValue[MIN] - 0.5 * rulerStep;
-        for (int i = 0; i < d.keys.length; i++) {
-            if (d.keys[i].length() >= 1) {
-                double x = xValue[MIN] + 1 + i;
-                StdDraw.text(x, y, d.keys[i]);
-            }
-        }
-    }
+    protected abstract void plotKeys();
     
     protected abstract void plotLegends();
 
-    private void plotBorder() {
-        double x = .5 * (xValue[MIN] + xValue[MAX]);
-        double y = .5 * (yValue[MIN] + yValue[MAX]);
-        double halfWidth = .5 * (xValue[MAX] - xValue[MIN]);
-        double halfHeight = .5 * (yValue[MAX] - yValue[MIN]);
-        StdDraw.setPenColor(f.borderColor);
-        StdDraw.rectangle(x, y, halfWidth, halfHeight);
-    }
+    protected abstract void plotBorder();
 
-    private void plotRightRuler() {
-        StdDraw.setPenColor(f.rulerColor);
-        final double x0 = xValue[MAX] - 0.05, x1 = xValue[MAX] + 0.05;
-        String[] mark = new String[rulerGrade + 1];
-        for (int i = 0; i <= rulerGrade; i++) {
-            double y = yValue[MIN] + i * rulerStep;
-            mark[i] = numberForRuler(y);
-            StdDraw.line(x0, y, x1, y);
-        }
-    } // DONE
+    protected abstract void plotRightRuler();
 
-    private void plotHeader() {
-        StdDraw.setFont(f.headerFont);
-        StdDraw.setPenColor(f.headerColor);
-        if(f.headerLocation == null) {
-        	double x = .5 * (xScale[MIN] + xScale[MAX]);
-        	double y = .5 * (yValue[MAX] + yScale[MAX]);
-        	StdDraw.text(x, y, d.header);
-        }
-        else {
-        	StdDraw.text(f.headerLocation[0], f.headerLocation[1], d.header);
-        }
-    }
+    protected abstract void plotHeader();
 
-    private void plotFooter() {
-        StdDraw.setFont(f.footerFont);
-        StdDraw.setPenColor(f.footerColor);
-        if(f.footerLocation == null) {
-        	double x = .5 * (xScale[MIN] + xScale[MAX]);
-        	double y = .5 * (yScale[MIN] + yValue[MIN]);
-        	StdDraw.text(x, y, d.footer);
-        }
-        else {
-        	StdDraw.text(f.footerLocation[0], f.footerLocation[1], d.footer);
-        }
-    }
+    protected abstract void plotFooter();
 
-    private final static int NORTH = 0;
-    private final static int SOUTH = 1;
-    private final static int WEST = 2;
-    private final static int EAST = 3;
-    private final static int MIN = 0;
-    private final static int MAX = 1;
+    protected final static int NORTH = 0;
+    protected final static int SOUTH = 1;
+    protected final static int WEST = 2;
+    protected final static int EAST = 3;
+    protected final static int MIN = 0;
+    protected final static int MAX = 1;
 }
--- finalProjectCS102A/src/TaskB/HistogramGrouped.java
+++ finalProjectCS102A/src/TaskB/HistogramGrouped.java
@@ -12,13 +12,154 @@ public class HistogramGrouped extends HistogramBase {
 		f.hasFooter = false;
 	}
 	
+    public void draw() {
+        setCanvas();
+        plotBars();
+        plotRuler();
+        plotKeys();
+        plotLegends();
+        if (f.hasBorder)
+            plotBorder();
+        if (f.hasRightRuler)
+            plotRightRuler();
+        if (f.hasHeader)
+            plotHeader();
+        if (f.hasFooter)
+            plotFooter();
+    }
+
+    protected void setCanvas() {
+        StdDraw.setCanvasSize(c.x, c.y);
+        setOriginalScale();
+        StdDraw.clear(c.bgColor);
+        StdDraw.setPenColor(c.color);
+    }
+
+    protected void setHistogramScale(int nBars) {
+        double span = yValue[MAX] - yValue[MIN] + 1;
+        double ySpacing = span / (1 - f.margins[NORTH] - f.margins[SOUTH]);
+        yScale[MIN] = yValue[MIN] - f.margins[SOUTH] * ySpacing - 1;
+        yScale[MAX] = yValue[MAX] + f.margins[NORTH] * ySpacing;
+        StdDraw.setYscale(yScale[MIN], yScale[MAX]);
+
+        double xSpacing = (nBars + 1) / (1 - f.margins[WEST] - f.margins[EAST]);
+        xScale[MIN] = -f.margins[WEST] * xSpacing - 1;
+        xScale[MAX] = nBars + f.margins[EAST] * xSpacing;
+        StdDraw.setXscale(xScale[MIN], xScale[MAX]);
+    };
+
+    protected void setOriginalScale() {
+        StdDraw.setXscale(c.xScale[MIN], c.xScale[MAX]);
+        StdDraw.setYscale(c.yScale[MIN], c.yScale[MAX]);
+    }
+
+
+    protected void plotRuler() {
+        StdDraw.setFont(f.rulerFont);
+        StdDraw.setPenColor(f.rulerColor);
+        final double x0 = xValue[MIN] - 0.05, x1 = xValue[MIN] + 0.05;
+        String[] mark = new String[rulerGrade + 1];
+        for (int i = 0; i <= rulerGrade; i++) {
+            double y = yValue[MIN] + i * rulerStep;
+            mark[i] = numberForRuler(y);
+            StdDraw.line(x0, y, x1, y);
+        }
+        int len = maxMarkLength(mark);
+        final double xs = xScale[MIN] + 0.7 * (xValue[MIN] - xScale[MIN]);
+        for (int i = 0; i <= rulerGrade; i++) {
+            double y = yValue[MIN] + i * rulerStep;
+            StdDraw.text(xs, y, String.format("%" + len + "s", mark[i]));
+        }
+    }
+
+    protected String numberForRuler(double x) { // TO BE Customized
+        if (yValue[MAX] >= 5 && rulerStep > 1)
+            return "" + (int) x;
+        if (rulerStep > 0.1)
+            return String.format("%.1f", x);
+        if (rulerStep > 0.01)
+            return String.format("%.2f", x);
+        if (rulerStep > 0.001)
+            return String.format("%.3f", x);
+        if (rulerStep > 0.0001)
+            return String.format("%.4f", x);
+        if (rulerStep > 0.00001)
+            return String.format("%.5f", x);
+        return String.format("%g", x);
+    }
+
+    private int maxMarkLength(String[] sa) {
+        int n = sa[0].length();
+        for (String s : sa)
+            if (n < s.length())
+                n = s.length();
+        return n;
+    }
+
+    protected void plotKeys() {
+        StdDraw.setFont(f.keysFont);
+        StdDraw.setPenColor(f.keyColor);
+        final double y = yValue[MIN] - 0.5 * rulerStep;
+        for (int i = 0; i < d.keys.length; i++) {
+            if (d.keys[i].length() >= 1) {
+                double x = xValue[MIN] + 1 + i;
+                StdDraw.text(x, y, d.keys[i]);
+            }
+        }
+    }
+    
+    protected void plotLegends() {
+    	
+    }
+
+    protected void plotBorder() {
+        double x = .5 * (xValue[MIN] + xValue[MAX]);
+        double y = .5 * (yValue[MIN] + yValue[MAX]);
+        double halfWidth = .5 * (xValue[MAX] - xValue[MIN]);
+        double halfHeight = .5 * (yValue[MAX] - yValue[MIN]);
+        StdDraw.setPenColor(f.borderColor);
+        StdDraw.rectangle(x, y, halfWidth, halfHeight);
+    }
+
+    protected void plotRightRuler() {
+        StdDraw.setPenColor(f.rulerColor);
+        final double x0 = xValue[MAX] - 0.05, x1 = xValue[MAX] + 0.05;
+        String[] mark = new String[rulerGrade + 1];
+        for (int i = 0; i <= rulerGrade; i++) {
+            double y = yValue[MIN] + i * rulerStep;
+            mark[i] = numberForRuler(y);
+            StdDraw.line(x0, y, x1, y);
+        }
+    } // DONE
+
+    protected void plotHeader() {
+        StdDraw.setFont(f.headerFont);
+        StdDraw.setPenColor(f.headerColor);
+        if(f.headerLocation == null) {
+        	double x = .5 * (xScale[MIN] + xScale[MAX]);
+        	double y = .5 * (yValue[MAX] + yScale[MAX]);
+        	StdDraw.text(x, y, d.header);
+        }
+        else {
+        	StdDraw.text(f.headerLocation[0], f.headerLocation[1], d.header);
+        }
+    }
+
+    protected void plotFooter() {
+        StdDraw.setFont(f.footerFont);
+        StdDraw.setPenColor(f.footerColor);
+        if(f.footerLocation == null) {
+        	double x = .5 * (xScale[MIN] + xScale[MAX]);
+        	double y = .5 * (yScale[MIN] + yValue[MIN]);
+        	StdDraw.text(x, y, d.footer);
+        }
+        else {
+        	StdDraw.text(f.footerLocation[0], f.footerLocation[1], d.footer);
+        }
+    }
 	
-	@Override
-	protected void plotRuler() {
+	
 
-	}
-
-	@Override
 	protected void plotBars() {
 		double[] a = d.values;
 		int n = a.length;
@@ -49,11 +190,6 @@ public class HistogramGrouped extends HistogramBase {
 
 	}
 
-	@Override
-	protected void plotLegends() {
-		// TODO Auto-generated method stub
-	}
-	
 	GroupedColor g = new GroupedColor(); // groupedcolor
 }
 
